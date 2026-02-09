f12308393479277ebdde445e9a34aa2b9bef5674
onkar <ovk0987@gmail.com>
2026-02-09 18:53:54 +0530

    New folder for online tutorials

---
+++ Inheritance/Demo.java
@@ -0,0 +1,30 @@
+package Inheritance;
+
+
+class A{
+    public void show(){
+        System.out.println("In A show");
+    }
+}
+
+class B extends A{
+    public void show(){
+        System.out.println("In B show");
+    }
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        // Inheritance - 06:10:51
+            // method overiding - 06:36:16
+        // B obj = new B();
+        // obj.show();
+
+
+    }
+
+}
+
+
+
+
